0acaed95e91aa02a041485172b90064eef29e44f
piyawan <mind.piyawan@gmail.com>
2014-01-29 14:49:56 +0700

    Edited by Piyawan

--- Lab11_1.php
+++ Lab11_1.php
@@ -1,6 +1,7 @@
 ﻿<? php 
-phpinfo();
-echo '<p>Hello World</p>';
-echo '<p>What a wonderful day</p>';
-echo '<p>We are having fun</p>';
+	phpinfo();
+	echo '<p>Hello World</p>';
+	echo '<p>What a wonderful day</p>';
+	echo '<p>We are having fun</p>';
+	echo '<p>My name is Piyawan</p>';
 ?>
